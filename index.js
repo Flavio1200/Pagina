c10b1c8fbf0bafd4769c99fec81db6cf7ec21aed
Flavio Ruiz <flavio.r.1200@gmail.com>
2022-06-17 15:30:36 -0400

    Agregando los archivos

---
+++ index.js
@@ -0,0 +1,5 @@
+const menu = document.querySelector('.menu');
+menu.addEventListener('click',function(){
+    let navegacion = document.querySelector('.navegacion');
+    navegacion.classList.toggle('aparece');
+})
